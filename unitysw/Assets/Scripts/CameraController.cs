ec2650cdad9a7b9fa5e83b52c04dd77e3758886d
Andrés Piñón Marín <a307737@uach.mx>
2021-05-01 23:18:01 -0600

    Movement added

---
+++ unitysw/Assets/Scripts/CameraController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform player;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+    }
+}
---
+++ unitysw/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Start() Variables
+    private Rigidbody2D rb;
+    private Animator pAnim;
+    private Collider2D pColl;
+    
+    //FSM
+    private enum animState { idle, running, jumping, falling, hurt, idleshoot, runningshoot}
+    private animState state = animState.idle;
+
+    //Inspector Variables
+
+    [SerializeField] private LayerMask ground;
+    [SerializeField] private float walkSpeed = 5f;
+    [SerializeField] private int jumpForce = 20;
+    [SerializeField] private float stuntForce = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        pAnim = GetComponent<Animator>();
+        pColl = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        animationState();
+        pAnim.SetInteger("state", (int)state);
+
+    }
+
+    private void animationState()
+    {
+        if (state == animState.jumping)
+        {
+            if (rb.velocity.y < .1f)
+            {
+                state = animState.falling;
+            }
+        }
+        else if (state == animState.falling)
+        {
+            if (pColl.IsTouchingLayers(ground))
+            {
+                state = animState.idle;
+            }
+        }
+        else if (Mathf.Abs(rb.velocity.x) > 2f)
+        {
+            //If velocity is greater than .1 in X axis is going to Right.
+            state = animState.running;
+        }
+        else
+        {
+            state = animState.idle;
+        }
+    }
+
+    private void Movement()
+    {
+        float hDirection = Input.GetAxis("Horizontal");
+
+        if (hDirection < 0)
+        {
+            rb.velocity = new Vector2(-walkSpeed, rb.velocity.y);
+            transform.localScale = new Vector2(-1, 1);
+        }
+        else if (hDirection > 0)
+        {
+            rb.velocity = new Vector2(walkSpeed, rb.velocity.y);
+            transform.localScale = new Vector2(1, 1);
+        }
+
+        // Jumping
+        if (Input.GetButtonDown("Jump") && pColl.IsTouchingLayers(ground))
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        state = animState.jumping;
+    }
+}
